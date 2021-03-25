e45308c01be6e82d9683d1a3d721ef583deeb040
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-03-24 20:55:20 -0500

    Atomic commit. As in the amount of code blew up like an atomic bomb without me commiting atomically.

---
+++ wishlist/travel_wishlist/functional_tests.py
@@ -0,0 +1,54 @@
+# These test aren't working, but aren't giving me any idea as to why. All I
+#  can guess is that it's where the selenium driver is stored, but I don't know.
+
+from selenium.webdriver.firefox.webdriver import WebDriver
+from django.test import LiveServerTestCase
+
+
+class TitleTest(LiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		super().setUpClass()
+		cls.selenium = WebDriver()
+		cls.selenium.implicitly_wait(10)
+
+	@classmethod
+	def tearDownClass(cls):
+		cls.selenium.quit()
+		super().tearDownClass()
+
+	def test_title_on_home_page(self):
+		self.selenium.get(self.live_server_url)
+		self.assertIn('Travel Wishlist', self.selenium.title)
+
+
+class AddPlacesTest(LiveServerTestCase):
+
+	fixtures = ['test_places']
+
+	@classmethod
+	def setUpClass(cls):
+		super().setUpClass()
+		cls.selenium = WebDriver()
+		cls.selenium.implicitly_wait(10)
+
+	@classmethod
+	def tearDownClass(cls):
+		cls.selenium.quit()
+		super().tearDownClass()
+
+	def test_add_new_place(self):
+		self.selenium.get(self.live_server_url)
+		input_name = self.selenium.find_element_by_id('id_name')
+		input_name.send_keys('Denver')
+
+		add_button = self.selenium.find_element_by_id('add-new-place')
+		add_button.click()
+
+		denver = self.selenium.find_element_by_id('place-name-5')
+		self.assertEqual('Denver', denver.text)
+
+		self.assertIn('Denver', self.selenium.page_source)
+		self.assertIn('New York', self.selenium.page_source)
+		self.assertIn('Tokyo', self.selenium.page_source)
